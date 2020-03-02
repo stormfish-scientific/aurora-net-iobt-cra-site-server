b1ac5c305f488def0bcedcc5750725e9e3ea77b2
Theron T. Trout <theron.trout@stormfish.io>
2020-03-02 17:03:33 -0500

    Added quick python file to demonstrate round trip of messages and checking for subfield presence

--- src/protobuf/prototest.py
+++ src/protobuf/prototest.py
@@ -10,3 +10,16 @@ phone_record.magnetic_field.z = 21.960579
 
 pprint(phone_record)
 
+encoded_data = phone_record.SerializeToString()
+
+pprint({'encoded_data': encoded_data})
+
+decoded_data = ucla_cellphone_telemetry_pb2.CellPhoneTelemetry()
+decoded_data.ParseFromString(encoded_data)
+
+pprint({'decoded_data': decoded_data})
+
+print('Has event_time    : ' + str(decoded_data.event_time != ''))
+print('Has timestamp     : ' + str(decoded_data.timestamp != 0.0))
+print('Has magnetic_field: ' + str(decoded_data.HasField('magnetic_field')))
+print('Has gyroscope     : ' + str(decoded_data.HasField('gyroscope')))
