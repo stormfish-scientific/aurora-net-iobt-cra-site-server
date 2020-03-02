269cdcbdd7864468a24ea90ca4f2126982f6d85d
Theron T. Trout <theron.trout@stormfish.io>
2020-03-02 15:53:54 -0500

    Added UCLA Cell Phone Telemetry protobuf messaging.

--- src/protobuf/make_py.sh
+++ src/protobuf/make_py.sh
@@ -2,3 +2,8 @@
 
 protoc --python_out=. intersite_management.proto
 cp intersite_management_pb2.py ../client
+
+protoc --python_out=. ucla_cellphone_telemetry.proto
+cp ucla_cellphone_telemetry_pb2.py ../client
+
+protoc --java_out=. ucla_cellphone_telemetry.proto
